4fa7486b2c74c6977c1c0c94837125aaaabf1636
Arielle Farhi <ariel.farhi@stu.shenkar.ac.il>
2026-08-18 13:24:02 +0300

    Add device model

---
+++ src/device.py
@@ -0,0 +1,5 @@
+class Device:
+    def __init__(self, model, ios_version, battery_level):
+        self.model = model
+        self.ios_version = ios_version
+        self.battery_level = battery_level
